1a99db52e98050dba609e77570f1474ad93855a0
Satwik Gupta <151006521+iamsatwik-dev@users.noreply.github.com>
2026-05-24 02:30:47 +0530

    Configure app for deployment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
